a801de761f34b15d90ee3df4a493538ee3189875
Adrián <adrianarana321@gmail.com>
2024-02-14 09:01:21 +0100

    Modificacion 1, Ejercicio1

---
+++ Ejercicio1/Circle.java
@@ -0,0 +1,29 @@
+package Práctica1.Ejercicio1;
+
+public class Circle {
+    private double radius;
+
+    private String color;
+
+    //Constructores
+    public Circle() {
+        radius=1.0;
+        color="red";
+
+    }
+
+    public Circle(double r) {
+        radius=r;
+        color="red";
+    }
+
+
+    //Metodos
+    public double getRadius (){
+        return radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+}
+//No se puede ejecutar la clase creada porque no accedemos a nada de ella desde un archivo "Main"
