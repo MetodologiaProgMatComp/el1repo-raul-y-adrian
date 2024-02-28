e14a2e4d0b49c5c551ca4a6562ba42138f78245f
Adrián <adrianarana321@gmail.com>
2024-02-28 08:06:29 +0100

    Clase book

---
+++ src/es/uah/matcomp/mp/el1/practica2Clases/Ejercicio1/Book.java
@@ -0,0 +1,59 @@
+package es.uah.matcomp.mp.el1.practica2Clases.Ejercicio1;
+
+public class Book {
+    private String name;
+
+    private  Author author;
+
+    private  double price;
+
+    private int qty;
+
+    //Contructor 1, sin cantidad
+    public Book(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+    //Constructor 2, con cantidad
+    public Book(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qtyGiven) {
+        this.qty=qtyGiven;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[" +
+                "name='" + name + '\'' +
+                ", author=" + author.toString() +
+                ", price=" + price +
+                ", qty=" + qty +
+                ']';
+    }
+}
--- src/es/uah/matcomp/mp/el1/practica2Clases/Ejercicio1/Main.java
+++ src/es/uah/matcomp/mp/el1/practica2Clases/Ejercicio1/Main.java
@@ -1,10 +1,25 @@
 package es.uah.matcomp.mp.el1.practica2Clases.Ejercicio1;
 
 public class Main {
-    Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm'); // Test the constructor
-System.out.println(ahTeck.toString()); // Test toString()
-ahTeck.setEmail("paulTan@nowhere.com"); // Test setter
-System.out.println("name is: " + ahTeck.getName()); // Test getter
-System.out.println("eamil is: " + ahTeck.getEmail()); // Test getter
-System.out.println("gender is: " + ahTeck.getGender()); // Test
+    public static void main(String[] args){
+        // Construct an author instance
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck); // Author's toString()
+        Book dummyBook = new Book("Java for dummy", ahTeck, 19.95, 99); // Test Book's Constructor
+        System.out.println(dummyBook); // Test Book's toString()
+        // Test Getters and Setters
+        dummyBook.setPrice(29.95);
+        dummyBook.setQty(28);
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("Author is: " + dummyBook.getAuthor()); // Author's toString()
+        System.out.println("Author's name is: " + dummyBook.getAuthor().getName());
+        System.out.println("Author's email is: " + dummyBook.getAuthor().getEmail());
+        // Use an anonymous instance of Author to construct a Book instance
+        Book anotherBook = new Book("more Java",
+                new Author("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
+        System.out.println(anotherBook); // toString()
+    }
+
 }
