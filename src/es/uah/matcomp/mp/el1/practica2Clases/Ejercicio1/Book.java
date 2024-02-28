ddecc22930dac68dbf0712b4bd0cf495ae6921b9
RaulMunozYelamos <158213974+RaulMunozYelamos@users.noreply.github.com>
2024-02-28 09:39:36 +0100

    comiteo

--- src/es/uah/matcomp/mp/el1/practica2Clases/Ejercicio1/Book.java
+++ src/es/uah/matcomp/mp/el1/practica2Clases/Ejercicio1/Book.java
@@ -1,68 +1,72 @@
 package es.uah.matcomp.mp.el1.practica2Clases.Ejercicio1;
 
+import java.util.Arrays;
+
 public class Book {
     private String name;
 
-    private  Author author;
-
     private  double price;
+    private Author[] authors;
 
     private int qty;
 
-    //Contructor 1, sin cantidad
-    public Book(String name, Author author, double price) {
-        this.name = name;
-        this.author = author;
-        this.price = price;
-    }
+
     //Constructor 2, con cantidad
-    public Book(String name, Author author, double price, int qty) {
+
+    public Book(String name,double price, Author[] authors) {
         this.name = name;
-        this.author = author;
         this.price = price;
+        this.authors = authors;
+    }
+
+    public Book(String name, double price, Author[] authors, int qty) {
+        this.name = name;
+        this.price = price;
+        this.authors = authors;
         this.qty = qty;
+    }
+
+    public Book(String javaForDummy, int price, String cdvfbgn, int qty) {
+    }
+
+
+    public Author[] getAuthors() {
+        return authors;
     }
 
     public String getName() {
         return name;
     }
 
-    public Author getAuthor() {
-        return author;
-    }
-
     public double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     public int getQty() {
         return qty;
     }
 
-    public void setQty(int qtyGiven) {
-        this.qty=qtyGiven;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public String getAuthorName(){
-        return author.getName();
+    public void setQty(int qty) {
+        this.qty = qty;
     }
-    public String getAuthorEmail(){
-        return author.getEmail();
-    }
-    public char getAuthorGender(){
-        return author.getGender();
-    }
+
     @Override
     public String toString() {
         return "Book[" +
-                "name='" + name + '\'' +
-                ", author=" + author.toString() +
+                "name= " + name + '\'' +
                 ", price=" + price +
+                ", authors=" + Arrays.toString(authors) +
                 ", qty=" + qty +
-                ']';
+                '}';
+    }
+    public String getAuthorNames(){
+        String nombre0= authors[0].getName();
+        String nombre1= authors[1].getName();
+
+        return "Los nombres son: " +nombre0+" y "+nombre1;
     }
 }
