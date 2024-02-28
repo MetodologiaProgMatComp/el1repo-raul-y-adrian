05861872ea2840d5e7711d41c65f25a304d4b66f
RaulMunozYelamos <158213974+RaulMunozYelamos@users.noreply.github.com>
2024-02-28 09:42:45 +0100

    comit nuevo

---
+++ src/es/uah/matcomp/mp/el1/practica2Clases/Ejercicio2/Author.java
@@ -0,0 +1,31 @@
+package es.uah.matcomp.mp.el1.practica2Clases.Ejercicio2;
+
+public class Author {
+    private String name;
+    private String email;
+
+    public Author(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Author[" +
+                "name= " + name +
+                ", email= " + email +
+                ']';
+    }
+}
