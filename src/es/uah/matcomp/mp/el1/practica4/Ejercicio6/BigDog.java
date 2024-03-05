ad1893a4b1c9b302360bc872dc20c8bdfd20624d
Adrián <adrianarana321@gmail.com>
2024-03-05 19:01:46 +0100

    sdfdgh

--- src/es/uah/matcomp/mp/el1/practica4/Ejercicio6/BigDog.java
+++ src/es/uah/matcomp/mp/el1/practica4/Ejercicio6/BigDog.java
@@ -4,4 +4,13 @@ public class BigDog extends Dog{
     public BigDog(String name){
         super(name);
     }
+    public void greets(){
+        System.out.println("wof");
+    }
+    public  void greets(Dog another){
+        System.out.println("woooooooooof");
+    }
+    public void greets(BigDog another){
+        System.out.println("WOOOOOOOOOOOOOOOOOOW");
+    }
 }
--- src/es/uah/matcomp/mp/el1/practica4/Ejercicio6/Main.java
+++ src/es/uah/matcomp/mp/el1/practica4/Ejercicio6/Main.java
@@ -1,4 +1,17 @@
 package es.uah.matcomp.mp.el1.practica4.Ejercicio6;
 
 public class Main {
+    public static void main(String[] args){
+        Cat cat=new Cat("Hola");
+
+        cat.greets();
+
+        Dog dog=new Dog("Paco");
+        dog.greets(dog);
+        Dog dog1=new Dog("sd");
+        dog1.greets(dog1);
+
+        BigDog dog2=new BigDog("sdcv");
+        dog2.greets(dog2);
+    }
 }
